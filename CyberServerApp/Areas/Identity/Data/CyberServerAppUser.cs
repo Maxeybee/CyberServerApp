6fec1a75fe1f58f128efd8009a8a2ef274c3e7fe
Et Ld <etienne.ludot@outlook.fr>
2022-12-22 05:27:00 -0800

    update code

--- CyberServerApp/Areas/Identity/Data/CyberServerAppUser.cs
+++ CyberServerApp/Areas/Identity/Data/CyberServerAppUser.cs
@@ -5,5 +5,6 @@ namespace CyberServerApp.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the CyberServerAppUser class
 public class CyberServerAppUser : IdentityUser
 {
+    public string CyberUserName { get; set; }
 }
 
--- CyberServerApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CyberServerApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,6 +80,11 @@ namespace CyberServerApp.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+            
+            [Required]
+            [Display(Name = "Username")]
+            public string CyberUsername { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
